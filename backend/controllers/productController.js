a5683da31c18208304a6b48b1e52090106b7e6a9
Sajid Malik <sajidmalik1661@hotmail.com>
2025-10-22 07:33:02 +0500

    feat: add basic product controller structure with CRUD function placeholders

---
+++ backend/controllers/productController.js
@@ -0,0 +1,24 @@
+
+
+// function for add product
+const addProduct=async (req,res)=>{
+
+}
+
+// function for List product
+const listProducts=async (req,res)=>{
+    
+}
+
+// function for remove product
+const removeProduct=async (req,res)=>{
+    
+}
+
+// function for single product info
+const singleProduct=async (req,res)=>{
+    
+}
+
+export {addProduct,listProducts,removeProduct,singleProduct};
+
